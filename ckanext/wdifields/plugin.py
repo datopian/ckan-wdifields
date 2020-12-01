7d79e9be21b097b99d7584277f6bc45260e72db2
Jake Ross <jirhiker@gmail.com>
2020-12-01 13:33:48 -0700

    clean up package creation

--- ckanext/wdifields/plugin.py
+++ ckanext/wdifields/plugin.py
@@ -2,13 +2,21 @@
 
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
-from ckan.plugins.toolkit import Invalid
+
+# from ckan.plugins.toolkit import Invalid
+
+IGNORE_MISSING = tk.get_validator('IGNORE_MISSING')
+CONVERT_TO_EXTRAS = tk.get_converter('convert_to_extras')
+CONVERT_FROM_EXTRAS = tk.get_converter('convert_from_extras')
+EMAIL_VALIDATOR = tk.get_validator('email_validator')
+NOT_EMPTY = tk.get_validator('not_empty')
+UNICODE = tk.get_validator('unicode_safe')
 
 
-def is_required(value):
-    if len(value) < 1:
-        raise Invalid("Is required.")
-    return value
+# def is_required(value):
+#     if len(value) < 1:
+#         raise Invalid("Is required.")
+#     return value
 
 
 class WdifieldsPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
@@ -18,214 +26,56 @@ class WdifieldsPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     def create_package_schema(self):
         # let's grab the default schema in our plugin
         schema = super(WdifieldsPlugin, self).create_package_schema()
-
-        schema.update(
-            {'update_interval_months': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')]})
-        # our custom field
-        schema.update({
-            'known_uses_of_data': [tk.get_validator('ignore_missing'),
-                                   tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_procedures': [tk.get_validator('ignore_missing'),
-                                           tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_quality_procedures': [tk.get_validator('ignore_missing'),
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_owner_organization': [is_required,
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_owner_contact_name': [is_required,
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_owner_contact_email': [tk.get_validator('email_validator'),
-                                         tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_organization': [tk.get_validator('ignore_missing'),
-                                             tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_contact_name': [tk.get_validator('ignore_missing'),
-                                             tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_contact_email': [tk.get_validator('ignore_missing'),
-                                              tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_qaqc_organization': [tk.get_validator('ignore_missing'),
-                                       tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_qaqc_contact_name': [tk.get_validator('ignore_missing'),
-                                       tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_qaqc_contact_email': [tk.get_validator('ignore_missing'),
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'funding_agency_organization': [tk.get_validator('ignore_missing'),
-                                            tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'funding_agency_contact_name': [tk.get_validator('ignore_missing'),
-                                            tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'funding_agency_contact_email': [tk.get_validator('ignore_missing'),
-                                             tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'coordinate_system': [tk.get_validator('ignore_missing'),
-                                  tk.get_converter('convert_to_extras')]
-        })
-        return schema
+        return self._custom_schema_hook(schema)
 
     def update_package_schema(self):
         schema = super(WdifieldsPlugin, self).update_package_schema()
-        # our custom field
+        return self._custom_schema_hook(schema)
+
+    def _custom_schema_hook(self, schema):
         schema.update(
-            {'update_interval_months': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')]})
-        schema.update({
-            'known_uses_of_data': [tk.get_validator('ignore_missing'),
-                                   tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_procedures': [tk.get_validator('ignore_missing'),
-                                           tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_quality_procedures': [tk.get_validator('ignore_missing'),
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_owner_organization': [tk.get_validator('ignore_missing'),
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_owner_contact_name': [tk.get_validator('ignore_missing'),
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_owner_contact_email': [tk.get_validator('ignore_missing'),
-                                         tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_organization': [tk.get_validator('ignore_missing'),
-                                             tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_contact_name': [tk.get_validator('ignore_missing'),
-                                             tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_collection_contact_email': [tk.get_validator('ignore_missing'),
-                                              tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_qaqc_organization': [tk.get_validator('ignore_missing'),
-                                       tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_qaqc_contact_name': [tk.get_validator('ignore_missing'),
-                                       tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'data_qaqc_contact_email': [tk.get_validator('ignore_missing'),
-                                        tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'funding_agency_organization': [tk.get_validator('ignore_missing'),
-                                            tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'funding_agency_contact_name': [tk.get_validator('ignore_missing'),
-                                            tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'funding_agency_contact_email': [tk.get_validator('ignore_missing'),
-                                             tk.get_converter('convert_to_extras')]
-        })
-        schema.update({
-            'coordinate_system': [tk.get_validator('ignore_missing'),
-                                  tk.get_converter('convert_to_extras')]
-        })
+            {
+                'title': [NOT_EMPTY, CONVERT_TO_EXTRAS, UNICODE],
+                'update_interval_months': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'known_uses_of_data': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'data_collection_procedures': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'data_quality_procedures': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'data_owner_organization': [NOT_EMPTY, CONVERT_TO_EXTRAS, UNICODE],
+                'data_owner_contact_name': [NOT_EMPTY, CONVERT_TO_EXTRAS, UNICODE],
+                'data_owner_contact_email': [EMAIL_VALIDATOR, CONVERT_TO_EXTRAS, UNICODE],
+                'data_collection_organization': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'data_collection_contact_name': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'data_collection_contact_email': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE, EMAIL_VALIDATOR],
+                'data_qaqc_organization': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'data_qaqc_contact_name': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'data_qaqc_contact_email': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE, EMAIL_VALIDATOR],
+                'funding_agency_organization': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'funding_agency_contact_name': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE],
+                'funding_agency_contact_email': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE, EMAIL_VALIDATOR],
+                'coordinate_system': [IGNORE_MISSING, CONVERT_TO_EXTRAS, UNICODE]
+            })
         return schema
 
     def show_package_schema(self):
-        schema = super(WdifieldsPlugin, self).show_package_schema()
-        schema.update(
-            {'update_interval_months': [tk.get_converter('convert_from_extras'), tk.get_validator('ignore_missing')]})
+        schema = super(WdifieldsPlugin, self).show_package_schema(),
+
         schema.update({
-            'known_uses_of_data': [tk.get_converter('convert_from_extras'),
-                                   tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_collection_procedures': [tk.get_converter('convert_from_extras'),
-                                           tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_quality_procedures': [tk.get_converter('convert_from_extras'),
-                                        tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_owner_organization': [tk.get_converter('convert_from_extras'),
-                                        tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_owner_contact_name': [tk.get_converter('convert_from_extras'),
-                                        tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_owner_contact_email': [tk.get_converter('convert_from_extras'),
-                                         tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_collection_organization': [tk.get_converter('convert_from_extras'),
-                                             tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_collection_contact_name': [tk.get_converter('convert_from_extras'),
-                                             tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_collection_contact_email': [tk.get_converter('convert_from_extras'),
-                                              tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_qaqc_organization': [tk.get_converter('convert_from_extras'),
-                                       tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_qaqc_contact_name': [tk.get_converter('convert_from_extras'),
-                                       tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'data_qaqc_contact_email': [tk.get_converter('convert_from_extras'),
-                                        tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'funding_agency_organization': [tk.get_converter('convert_from_extras'),
-                                            tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'funding_agency_contact_name': [tk.get_converter('convert_from_extras'),
-                                            tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'funding_agency_contact_email': [tk.get_converter('convert_from_extras'),
-                                             tk.get_validator('ignore_missing')]
-        })
-        schema.update({
-            'coordinate_system': [tk.get_converter('convert_from_extras'),
-                                  tk.get_validator('ignore_missing')]
+            'known_uses_of_data': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_collection_procedures': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_quality_procedures': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_owner_organization': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_owner_contact_name': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_owner_contact_email': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_collection_organization': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_collection_contact_name': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_collection_contact_email': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_qaqc_organization': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_qaqc_contact_name': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'data_qaqc_contact_email': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'funding_agency_organization': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'funding_agency_contact_name': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'funding_agency_contact_email': [CONVERT_FROM_EXTRAS, IGNORE_MISSING],
+            'coordinate_system': [CONVERT_FROM_EXTRAS, IGNORE_MISSING]
         })
         return schema
 
